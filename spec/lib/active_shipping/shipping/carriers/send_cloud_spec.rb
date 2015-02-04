896a95e1d26f98133266acf9c972793b5fe205c5
Artyom Lagun <artyom.lagun@gmail.com>
2015-02-04 18:30:51 +0300

    write test

--- spec/lib/active_shipping/shipping/carriers/send_cloud_spec.rb
+++ spec/lib/active_shipping/shipping/carriers/send_cloud_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SpreeSendcloud::SendCloud do
+describe ActiveShipping::Shipping::Carriers::SendCloud do
   let!(:location) do
     {
         country_code: 'NL',
@@ -14,7 +14,7 @@ describe SpreeSendcloud::SendCloud do
   context 'Sendcloud' do
     context 'find rates' do
       it 'with valid params' do
-        response = SpreeSendcloud::SendCloud.find_rates(location, location, nil)
+        response = SendCloud.find_rates(location, location, nil)
         expect(response).not_to be_empty
       end
     end
