0be7c3935e4ea44eef7206cdfc93792bc174b4a5
fesstor <emelya1993@rambler.ru>
2015-03-03 10:54:20 +0300

    Fix shipping methods names

--- spec/models/sendcloud_calculator_spec.rb
+++ spec/models/sendcloud_calculator_spec.rb
@@ -59,7 +59,7 @@ module ActiveShipping
     describe "available" do
       context "when rates are available" do
         let(:rates) do
-          [ double('rate', :service_name => 'Pakket Nederland (PostNL)', :service_code => "Pakket Nederland (PostNL)", :price => 1) ]
+          [ double('rate', :service_name => 'PostNL Nederland', :service_code => "PostNL Nederland", :price => 1) ]
         end
 
         before do
@@ -111,7 +111,7 @@ module ActiveShipping
 
     describe "compute" do
       let(:rates) do
-        [ double('rate', :service_name => 'Pakket Nederland (PostNL)', :service_code => "Pakket Nederland (PostNL)", :price => 999) ]
+        [ double('rate', :service_name => 'PostNL Nederland', :service_code => "PostNL Nederland", :price => 999) ]
       end
 
       context "with valid response" do
